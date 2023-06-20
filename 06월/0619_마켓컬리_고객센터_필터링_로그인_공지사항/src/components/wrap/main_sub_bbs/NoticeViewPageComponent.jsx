7e24b5e05b3b83e417fedbae78cfd400fe1c51e7
soyeon98 <212psy@naver.com>
2023-06-20 18:05:52 +0900

    0620

---
+++ 06월/0619_마켓컬리_고객센터_필터링_로그인_공지사항/src/components/wrap/main_sub_bbs/NoticeViewPageComponent.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import './notice_scss/notice_view.scss';
+
+export default function NoticeViewPageComponent () {
+    return (
+        <div id='noticeView'>
+            <div className="container">
+                <div className="title">
+                    <h2>공지사항</h2>
+                    <p>컬리의 새로운 소식들과 유용한 정보들을 한곳에서 확인하세요.</p>
+                </div>
+                <div className="content">
+                    <ul>
+                        <li>
+                            <div className="left">제목</div>
+                            <div className="right">[안내] 컬리 소비자 분쟁해결 기준 안내</div>
+                        </li>
+                        <li>
+                            <div className="left">작성자</div>
+                            <div className="right">컬리</div>
+                        </li>
+                        <li>
+                            <div className="left">작성일</div>
+                            <div className="right">2022.07.27</div>
+                        </li>
+                        <li>
+                            <div className="contents">
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ullam ex placeat nostrum nihil similique obcaecati, distinctio autem quod aliquam maxime quibusdam fugiat. Laborum aperiam magni ipsum ab pariatur reprehenderit?</p>
+                                <p>Officiis, aliquid unde ipsa iure aliquam cum distinctio doloribus ipsam maxime voluptas temporibus libero molestias repudiandae quidem hic iusto velit. Inventore, tempora obcaecati nesciunt sequi eaque at officiis corrupti molestias?</p>
+                                <p>Distinctio et molestiae nisi aliquam minus saepe mollitia impedit eveniet, nobis corporis sapiente explicabo doloribus recusandae delectus doloremque vero quas esse officia amet expedita. Expedita, quaerat! Illo id suscipit blanditiis!</p>
+                                <p>Illum asperiores debitis repellat quisquam assumenda voluptatum vel quasi iusto laborum adipisci quia dolorem perferendis blanditiis magnam in, aliquam porro qui officia repudiandae error repellendus voluptas eos! Laudantium, maxime blanditiis!</p>
+                                <p>Cum aliquam, eum laborum magnam, quasi iusto culpa illo eaque, rem in reiciendis pariatur dolor. Ipsum dignissimos iste atque culpa sint incidunt asperiores blanditiis fuga corporis, deleniti, eligendi doloribus nostrum?</p>
+                                <p>Enim cum consectetur minus esse fuga ipsam quam odio corporis recusandae alias veritatis earum eveniet incidunt voluptates assumenda quia at, quisquam consequatur ratione! Mollitia aliquid dignissimos praesentium similique ullam saepe!</p>
+                                <p>Fuga ex optio non sed magnam minus architecto natus mollitia possimus ullam sunt consectetur assumenda illum, beatae maiores itaque debitis aliquid, earum id omnis ut! Possimus cum ullam illum iure.</p>
+                                <p>Ea ducimus minima sequi earum laboriosam aspernatur velit accusamus voluptatem libero accusantium omnis, saepe, dolorem, aut voluptates pariatur rerum vel tempore sapiente. Sunt, non harum dolorem commodi voluptates exercitationem est!</p>
+                                <p>Expedita sint aliquid aliquam quidem magni dolores nisi iste autem fugiat animi repellat officia vitae non soluta voluptatum, similique numquam esse quos sapiente odio. Ut delectus quo porro! Non, impedit!</p>
+                                <p>Laboriosam quasi inventore enim nulla asperiores nesciunt exercitationem accusamus explicabo repellendus eaque! Voluptatibus ipsa numquam ipsum eos ducimus molestias, aut a repellat eveniet, maiores nobis esse labore provident quibusdam beatae.</p>
+                                <p>Corrupti doloribus eaque facere perferendis ducimus expedita optio vitae pariatur ipsa modi dolorem obcaecati aliquid reiciendis quae quod voluptatum deleniti maiores, enim deserunt autem eius cum vero quaerat? Excepturi, odio!</p>
+                                <p>Quam nulla iusto repellat id deleniti laboriosam, perferendis exercitationem mollitia eveniet qui nesciunt similique architecto, obcaecati culpa esse sit distinctio et voluptatibus totam odio, pariatur repudiandae minus. Praesentium, minima est!</p>
+                                <p>Minima at praesentium asperiores esse sed iusto harum labore tempore ipsa architecto vel aliquam, delectus velit quos eos minus animi cupiditate quidem, optio dolorum eum quae? Rem quas labore sequi.</p>
+                                <p>Quisquam accusantium molestiae iusto sed culpa in, fugiat itaque natus debitis deserunt perspiciatis. Ducimus consequatur atque molestias harum, alias magnam aut, sed illo suscipit laborum maiores omnis unde qui exercitationem.</p>
+                                <p>Nihil dolor esse in quasi debitis qui tenetur maiores corporis error consequuntur rem illo facilis molestiae quisquam unde culpa nulla numquam sequi, iusto vero doloremque velit alias provident eaque. Magni.</p>
+                                <p>Rem, magnam eum esse enim nobis non maiores placeat consequatur voluptatibus culpa deleniti fuga voluptatem animi impedit. Ullam odio nulla magni officiis. Fugiat necessitatibus perferendis vitae adipisci iusto debitis earum.</p>
+                                <p>Vitae mollitia molestias corporis at eum cumque veritatis maiores provident fugit, minus explicabo velit magnam laboriosam dolor. In laborum sapiente iure consequatur cumque eos! Porro enim pariatur omnis rem dicta.</p>
+                                <p>Soluta, aliquam hic. Aut tempore non, consequatur doloremque eius, totam sunt accusamus vitae inventore quasi incidunt cum excepturi earum repellendus odio facilis consequuntur quibusdam eum ut, quisquam ad? Voluptatibus, maiores.</p>
+                                <p>Quae voluptatibus exercitationem asperiores quos, at qui sed molestiae! Quasi, itaque. Culpa ducimus delectus reiciendis vitae, quod laborum suscipit provident unde, ex impedit quibusdam atque, minima enim ullam cumque iure.</p>
+                                <p>Esse amet ipsum fuga quo est, sit numquam eligendi accusantium error aut quis molestiae aliquid incidunt nobis nesciunt saepe necessitatibus eveniet quisquam placeat similique. Laborum dicta quo minus natus quia.</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
