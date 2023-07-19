01b912b7db06cbf1140d07399ca669dc7afc6953
soyeon98 <212psy@naver.com>
2023-07-19 10:03:10 +0900

    7월달꺼 옯기기

--- 07월/0712_스프링부트_NVC_MODELING_entity_repository/app/src/main/java/com/example/app/controller/UserDTOEntityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.app.controller;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserDTOEntityRepository extends CrudRepository<UserDTOEntity,Long> {
-}
--- 07월/0712_스프링부트_NVC_MODELING_entity_repository/task/src/main/java/com/example/task/controller/UserDTOEntityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.task.controller;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserDTOEntityRepository extends CrudRepository<UserDTOEntity,Long> {
-}
